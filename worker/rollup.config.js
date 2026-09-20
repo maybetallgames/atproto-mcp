86e468bde3df0a351581906095480d587f416174
MaybeTall <eric.r.fraze@gmail.com>
2026-09-20 03:17:10 -0400

    Add OAuth Worker bundling configuration

---
+++ worker/rollup.config.js
@@ -0,0 +1,16 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "es",
+    sourcemap: false,
+  },
+  plugins: [
+    nodeResolve({ exportConditions: ["worker", "browser", "import", "default"] }),
+    typescript({ target: "ES2022", module: "ESNext" }),
+  ],
+};
+
